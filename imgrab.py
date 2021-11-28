86bee80dc2377456dfd99e7e5345e39f96f906bf
PamekasanCode <69365920+pamekasancode@users.noreply.github.com>
2021-11-28 09:43:32 +0700

    new sc

--- imgrab.py
+++ imgrab.py
@@ -1,62 +1,57 @@
 # copyright pamekasancode
-import os, sys, requests
+import os, requests
 from bs4 import BeautifulSoup as bs
-b = "\033[96m"
-m = "\033[91m"
-p = "\033[97m"
+
 def clear():
-	os.system("clear || cls")
-clear()
-def directory():
+	os.system("cls || clear")
+
+def banner():
+	clear()
+	print("""\033[91m
+ _____                          _     
+|_   _|                        | |    
+  | | _ __ ___   __ _ _ __ __ _| |__  
+  | || '_ ` _ \ / _` | '__/ _` | '_ \ 
+ _| || | | | | | (_| | | | (_| | |_) |
+ \___/_| |_| |_|\__, |_|  \__,_|_.__/  V.1.0
+                 __/ |                
+                |___/                 
+
+\033[90mAuthor: Pamekasancode Team
+
+\033[97m""")
+banner()
+
+def saveImg(nameImage, image):
+	fw = open(nameImage, "wb")
+	fw.write(image.content)
+	fw.close()
+def changeDir():
 	try:
-		os.chdir("/sdcard")
+		os.chdir("imgrab")
+	except:
 		os.mkdir("imgrab")
 		os.chdir("imgrab")
-	except OSError:
-		os.chdir("/sdcard/imgrab")
-	except OSError:
-		os.mkdir("imgrab")
-		os.chdir("imgrab")
-print (f"""{b}
-     _____
-    (, /                         /)
-      / ___   _     _   __  _   (/_  _  __
-  ___/__// (_(_/_  (_/_/ (_(_(_/_) _(/_/ (_
-(__ /       .-/   .-/
-           (_/   (_/
-{p}Images Grab
-Author  : Pamekasancode Team
-""")
-url = input("Url	: ")
-directory()
-req = requests.get(url)
-soup = bs(req.text, "html.parser")
-tag = soup.find_all("img")
-for img in tag:
-	images = img.get("src")
-	if "http" in images:
-		image = requests.get(images)
-		namefile = images.split("/")[-1]
-		if len(namefile) > 15:
-			namefile = namefile[:8]
-			print (f"{b}[{p}~{b}] {p}Mendownload "+namefile+".png")
+def main(url):
+	changeDir()
+	r = requests.get(url)
+	soup = bs(r.text, "html.parser")
+	tag = soup.find_all('img')
+	for img in tag:
+		images = img.get('src')
+		nameImage = images .split("/")[-1]
+		if len(nameImage) > 13:
+			nameImage = nameImage[:13]
+		if not ".jpg" in nameImage:
+			nameImage = nameImage+".jpg"
+		if "http" in images:
+			image = requests.get(images)
+			saveImg(nameImage, image)
+			print("\033[92m[+] \033[97m"+nameImage)
 		else:
-			print (f"{b}[{p}~{b}] {p}Mendownload "+namefile)
-			fw = open(namefile, "wb")
-			fw.write(image.content)
-			fw.close()
-	elif not "http" in images:
-		image = requests.get(url+"/"+images)
-		namefile = images.split("/")[-1]
-		if len(namefile) > 15:
-			namefile = namefile[:8]
-			print (f"{b}[{p}~{b}] {p}Mendownload "+namefile+".png")
-		else:
-			print (f"{b}[{p}~{b}] {p}Mendownload "+namefile)
-			fw = open(namefile, "wb")
-			fw.write(image.content)
-			fw.close()
-	else:
-		print (f"{m}\n[x] Gambar Tidak Dapat Ditemukan{p}")
-pass
-print ("\n[√] Gambar disimpan di "+os.getcwd())
+			image = requests.get(url+"/"+images)
+			saveImg(nameImage, image)
+			print("\033[92m[+] \033[97m"+nameImage)
+link = input("Link Webpage: \033[92m")
+main(link)
+print ("\n\033[92m[√] Gambar disimpan di \033[97m"+os.getcwd())
